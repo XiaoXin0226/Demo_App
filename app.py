3a60b33ae75af08bdcee6c3f4d7c38d20283281b
XiaoXin0226 <wongth0w0226@gmail.com>
2026-04-20 17:45:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import streamlit as st
 
 st.title("Welcome to Streamlit!")
 
+st.header("Section 1: Introduction")
 st.write("Hello, Streamlit!")
 st.write(12345)
 st.write({"Name": "Alice", "Age": 30})
 
 st.write(['list item 1', 'list item 2'])
 
+st.header("Section 2: Markdown")
 st.write("**Bold Text** and *Italic Text*")
