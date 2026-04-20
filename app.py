b31b1b722612b2ec1952212b83c5ebb4882655bc
XiaoXin0226 <wongth0w0226@gmail.com>
2026-04-20 17:46:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,11 @@ st.write({"Name": "Alice", "Age": 30})
 
 st.write(['list item 1', 'list item 2'])
 
+age = st.number_input("Enter your age:",
+                      min_value=0,
+                      max_value=120,
+                      value=25)
+st.write(f"Your age is {age}")
+
 st.header("Section 2: Markdown")
 st.write("**Bold Text** and *Italic Text*")
