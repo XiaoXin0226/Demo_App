f3e0daeb54aef557491bc5bc74d320e8692a5269
XiaoXin0226 <wongth0w0226@gmail.com>
2026-04-20 17:40:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("Welcome to Streamlit!")
+
+st.write("Hello, Streamlit!")
+st.write(12345)
+st.write({"Name": "Alice", "Age": 30})
+
+st.write(['list item 1', 'list item 2'])
